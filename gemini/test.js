2113557ca8d6ab8903cf3e5b037f2b3e6c4da375
Alexander Savin <box.for.postman@gmail.com>
2017-04-10 12:36:30 +0300

    Remove coverage and add App wrapper for gemini tests

--- gemini/test.js
+++ gemini/test.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import App from '../src/components/App';
 import Header from '../src/components/Header';
 
 geminiReact.suite('Header', (suite) => {
-  suite.render(<Header />)
-        .capture('plain');
+  suite.render(<App context={{ insertCss: (...styles) => styles.forEach(x => x._insertCss()) }}>
+    <Header />
+  </App>)
+    .capture('plain');
 });
